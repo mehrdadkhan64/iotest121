947eee8cb21f4f851348b7aee34deb017c68b723
Glitch (sweet-chess) <none>
2018-06-13 06:45:05 +0000

    🍍🏡 Checkpoint ./src/App.js:1251465/1405

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Entity, Scene } from 'aframe-react';
+//import { Entity, Scene } from 'aframe-react';
 import  '@ajwest/aframe';
 
 import './face-notifications';
@@ -12,19 +12,13 @@ class App extends Component {
  
    
     <a-scene>
-      <a-entity id="sky" geometry="primitive: sphere; radius: 5000"
+     <div> <a-entity id="sky" geometry="primitive: sphere; radius: 5000"
                 material="shader: gradient; topColor: 235 235 245; bottomColor: 185 185 210"
-                scale="-1 1 1"></a-entity>
+                scale="-1 1 1"></a-entity> </div>
  
-       <a-entity id="sphere" geometry="primitive: sphere"
-                material="color: #EFEFEF; shader: flat"
-                position="0 0.15 -5"
-                light="type: point; intensity: 5"
-                animation="property: position; easing: easeInOutQuad; dir: alternate; dur: 1000; to: 0 -0.10 -5; loop: true"></a-entity>
- 
-      <a-entity id="ocean" ocean="density: 20; width: 50; depth: 50; speed: 4"
+      <div>     <a-entity id="ocean" ocean="density: 20; width: 50; depth: 50; speed: 4"
                 material="color: #9CE3F9; opacity: 0.75; metalness: 0; roughness: 1"
-                rotation="-90 0 0"></a-entity>
+                rotation="-90 0 0"></a-entity> </div>
       <a-entity id="light" light="type: ambient; color: #888"></a-entity>
       
       
