78e4ce38145b5f716d0960e2138bf0dc38427d81
Glitch (sweet-chess) <none>
2018-06-13 15:30:13 +0000

    🎺🏈 Checkpoint ./src/App.js:1251465/5231

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react' ;
 import { Entity, Scene } from 'aframe-react';
 import '@ajwest/aframe';
 import io3d from '3dio';
+import '3dio';
 
 
 
@@ -14,17 +15,51 @@ class App extends Component {
 
    
 
- // <script src="https://aframe.io/releases/0.7.1/aframe.min.js"></script>
- // <script src="https://dist.3d.io/3dio-js/1.1.x/3dio.min.js"></script>
-
-  <a-scene>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="theme-color" content="#000000">
     
-    <a-entity io3d-furniture="id:10344b13-d981-47a0-90ac-f048ee2780a6" position="-2 0 -3.2" rotation="0 180 0"></a-entity>
-
+    <link rel="manifest" href="%PUBLIC_URL%/manifest.json">
+    <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
+    <script src="https://aframe.io/releases/0.7.0/aframe.min.js"></script>
+    <script src="https://dist.3d.io/3dio-js/1.x.x/3dio.min.js"></script>
     
-    <a-entity io3d-data3d="key:/3f995099-d624-4c8e-ab6b-1fd5e3799173/170515-0913-4p3ktf/1e588a3b-90ac-4a32-b5b8-ff2fda7f87c4.gz.data3d.buffer" position="0 0 0"></a-entity>
-  </a-scene>
-
+    <title>Treasure Hunt</title>
+  
+    <noscript>
+      You need to enable JavaScript to run this app.
+    </noscript>
+    <div id="root">
+      <a-scene>
+        <a-entity io3d-data3d="url:https://storage.3d.io/535e624259ee6b0200000484/170725-1421-wujxjb/archilogic_2017-07-25_14-19-02_hn9axa.gz.data3d.buffer"
+                  position="6.031 0 0.145">
+          <a-camera id="camera" position="-6.085 0 0" rotation="0 0 0" wasd-controls="enabled: false">
+            <a-entity id="hud"></a-entity>
+            <a-entity cursor="fuse: true; fuseTimeout: 2000"
+                      raycaster="objects:.clickable; far: 3"
+                      position="0 0 -1"
+                      geometry="primitive: ring; radiusInner: 0.04; radiusOuter: 0.05"
+                      material="color: black; shader: flat">
+              <a-animation begin="click" easing="ease-in" attribute="scale" dur="150"
+                           fill="forwards" from="0.1 0.1 0.1" to="1 1 1"></a-animation>
+              <a-animation begin="cursor-fusing" easing="ease-in" attribute="scale" dur="2000"
+                           fill="backwards" from="1 1 1" to="0.1 0.1 0.1"></a-animation>
+            </a-entity>
+          </a-camera>
+          <a-entity walking-trail position="-6.085 0 0">
+            <a-entity walking-trail-item position="-0.441 0 -1.125"></a-entity>
+            <a-entity walking-trail-item position="-0.296 0 2.027"></a-entity>
+            <a-entity walking-trail-item position="-3.594 0 -1.190"></a-entity>
+            <a-entity walking-trail-item position="-3.594 0 -3.829"></a-entity>
+            <a-entity walking-trail-item position="-3.594 0 -4.883"></a-entity>
+            <a-entity walking-trail-item position="-2.001 0 -3.829"></a-entity>
+            <a-entity walking-trail-item position="0.095 0 -4.883"></a-entity>
+            <a-entity walking-trail-item position="3.020 0 -4.883"></a-entity>
+          </a-entity>
+        </a-entity>
+        <a-entity clues-component=""></a-entity>
+      </a-scene>
+    </div>
     );
   }
 }
