2759d5fed91ad38d54f3d28c4c5461f38d4089be
Glitch (sweet-chess) <none>
2018-06-13 06:02:18 +0000

    🐗🍼 Checkpoint ./src/App.js:1251465/730

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Entity, Scene } from 'aframe-react';
+import '@ajwest/aframe';
 
 import './face-notifications';
 
@@ -7,20 +8,18 @@ class App extends Component {
   render() {
     return (
       <Scene>
-        <Entity camera='user-height: 1.6' look-controls='' face-notifications=''>
-          <Entity
-          geometry={{primitive: 'box', width: 0.2, height: 0.2, depth: 0.2}} material={{color: 'blue'}}
-          position={{x: 0, y: 0, z: -1}} />
-        </Entity>
-        <Entity
-          geometry={{primitive: 'box'}} material={{color: 'red'}}
-          position={{x: 0, y: 0, z: -1}} />
+      
       
        <Entity id="sphere" geometry="primitive: sphere"
                 material="color: #EF2D5E; shader: flat"
                 position="0 0.15 -5"
                 light="type: point; intensity: 5"
                 animation="property: position; easing: easeInOutQuad; dir: alternate; dur: 1000; to: 0 -0.10 -5; loop: true"></Entity>
+      <a-entity id="sky" geometry="primitive: sphere; radius: 5000"
+                material="shader: gradient; topColor: 235 235 245; bottomColor: 185 185 210"
+                scale="-1 1 1"></a-entity>
+ 
+      <a-entity id="light" light="type: ambient; color: #888"></a-entity>
       
       </Scene>
     );
