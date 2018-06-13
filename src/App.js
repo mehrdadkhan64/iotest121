dcf072dbd707a5589244e102f300fc9c9ad0aa27
Glitch (sweet-chess) <none>
2018-06-13 07:48:09 +0000

    🍉🚆 Checkpoint ./src/App.js:1251465/954

--- src/App.js
+++ src/App.js
@@ -2,35 +2,23 @@ import React, { Component } from 'react';
 //import { Entity, Scene } from 'aframe-react';
 import  '@ajwest/aframe';
 import io3d from '3dio';
+import 'react';
 
 import './face-notifications';
 
 class App extends Component {
   render() {
     return (
-      <div>
-    
- 
+       
    
    <html>
-  <head>
-    <title>Default setup</title>
-    <meta name="description" content="3d.io residential interior scene with dynamic lighting and realtime shadows">
-    <!-- aframe library -->
-    <script src="https://aframe.io/releases/0.7.1/aframe.min.js"></script>
-    <!-- 3dio library. distributed via 'npm run dist' in https://github.com/archilogic-com/3dio-js -->
-    <script src="https://3d.io/releases/3dio-js/1.1.x/3dio.min.js"></script>
-    <link href="https://storage-nocdn.3d.io/app-creator-templates/default.css" rel="stylesheet">
-    <script src="https://unpkg.com/aframe-animation-component@3.2.5/dist/aframe-animation-component.min.js"></script>
-  </head>
+ 
   <body>
-    <!-- io3d identifier -->
+  //   io3d identifier -->
     <io3d-app id="default_setup" scene-id="62cb3510-6708-4f62-94c3-f9936db7e20b"></io3d-app>
-    <!-- ui -->
+   // < ui >
     <div id="custom-logo">
-      <a href="https://3d.io" target="_blank">
-        <img src="https://archilogic-com.github.io/ui-style-guide/3d-io-logo/3d-io-logo-small.svg">
-      </a>
+     
     </div>
     <div class="camera-controls">
       <div class="waypoints">
@@ -60,7 +48,7 @@ class App extends Component {
         </div>
       </div>
     </div>
-    <!-- 3d scene -->
+    // 3d scene -->
     <a-scene io3d-lighting="intensity:0.9">
       <a-entity camera="" tour="autoStart: false" wasd-controls="" look-controls="" position="-2.073 1.6 -1.474" rotation="-2.063 203.308 0">
         <a-entity tour-waypoint="Top View" io3d-uuid="add7b140-7563-463a-b324-75e2b460e915" position="-7.959815433483447 22.172638840745442 1.675735956699384" rotation="-89.9 450 0"></a-entity>
@@ -138,7 +126,7 @@ class App extends Component {
   </body>
 </html>
 
- </div>
+ 
     );
   }
 }
