ff34cbc7369b43276ebffc49803c163dd5b2f4a2
Glitch (sweet-chess) <none>
2018-06-13 06:13:04 +0000

    👟🌝 Checkpoint ./src/App.js:1251465/660

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Entity, Scene } from 'aframe-react';
-import '@ajwest/aframe';
+import  '@ajwest/aframe';
 
 import './face-notifications';
 
@@ -15,13 +15,17 @@ class App extends Component {
                 position="0 0.15 -5"
                 light="type: point; intensity: 5"
                 animation="property: position; easing: easeInOutQuad; dir: alternate; dur: 1000; to: 0 -0.10 -5; loop: true"></Entity>
+            
+      </Scene>
+
+    <a-scene>
       <a-entity id="sky" geometry="primitive: sphere; radius: 5000"
                 material="shader: gradient; topColor: 235 235 245; bottomColor: 185 185 210"
                 scale="-1 1 1"></a-entity>
  
       <a-entity id="light" light="type: ambient; color: #888"></a-entity>
+      </a-scene>
       
-      </Scene>
     );
   }
 }
