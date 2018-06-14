d297c0e2d5b1c2a9449d0cd35b4cae0da151866a
Glitch (sweet-chess) <none>
2018-06-14 02:38:37 +0000

    🚆🚉 Checkpoint ./src/App.js:1251465/27

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react' ;
 import { Entity, Scene } from 'aframe-react';
 import aframe from '@ajwest/aframe';
-const io3d = require('3dio');
+import io3d from '3dio';
 
 
 
