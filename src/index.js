30798d1f0e67be0656c5e5ac45af3baef7c88f51
Glitch (sweet-chess) <none>
2018-06-14 01:37:35 +0000

    🌧🎳 Checkpoint ./src/index.js:1251465/50 ./package.json:1251465/101

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import aframe from '@ajwest/aframe';
 import React from 'react';
-import io3d from '3dio';
 import ReactDOM from 'react-dom';
 import App from './App';
+import io3d from '3dio';
 
 ReactDOM.render(
   <App />,
