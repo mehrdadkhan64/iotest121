91c3cdf5328596a542ad3447bd3be5d91eb96dbe
Glitch (sweet-chess) <none>
2018-06-14 12:32:30 +0000

    🍋🌑 Checkpoint ./src/App.js:1251465/8

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ class App extends Component {
     </noscript>
     <div id="root">
       <Scene>
-        <a-entity io3d-data3d="url:https://storage.3d.io/535e624259ee6b0200000484/170725-1421-wujxjb/archilogic_2017-07-25_14-19-02_hn9axa.gz.data3d.buffer"
+        <Entity io3d-data3d="url:https://storage.3d.io/535e624259ee6b0200000484/170725-1421-wujxjb/archilogic_2017-07-25_14-19-02_hn9axa.gz.data3d.buffer"
                   position="6.031 0 0.145">
           <a-camera id="camera" position="-6.085 0 0" rotation="0 0 0" wasd-controls="enabled: false">
             <a-entity id="hud"></a-entity>
@@ -58,7 +58,7 @@ class App extends Component {
             <a-entity walking-trail-item position="0.095 0 -4.883"></a-entity>
             <a-entity walking-trail-item position="3.020 0 -4.883"></a-entity>
           </a-entity>
-        </a-entity>
+        </Entity>
         <a-entity clues-component=""></a-entity>
       </Scene>
     </div>
