15d464478ea4b5b4ed1860e16b19a31ed543121a
Glitch (sweet-chess) <none>
2018-06-30 06:39:29 +0000

    👮🎩 Checkpoint ./src/index.js:1251465/83

--- src/index.js
+++ src/index.js
@@ -21,8 +21,8 @@ import React, { Component } from 'react' ;
 //import aframe from '@ajwest/aframe';
 //import io3d from 'io3fix';
 //import 'aframe-animation-component';
-import io3d from'3dio';
-
+//import io3d from'3dio';
+var io3d = require("https://dist.3d.io/3dio-js/1.2.1/3dio.min.js")
 var a='/535e624259ee6b0200000484/bake/2017-06-30_11-05-49_P144IW/regular/lighting.gz.data3d.buffer';
 import ReactDOM from 'react-dom';
 //import {Box, Sphere, Cylinder, Plane, Sky, Text, Scene} from 'react-aframe-ar';
